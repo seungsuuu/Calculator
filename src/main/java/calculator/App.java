6838bd2c4420ca49b47c4cbe174a477a194ea260
seungsuuu <tmdtn013@naver.com>
2024-04-29 11:58:28 +0900

    level1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -2,20 +2,26 @@ package main.java.calculator;
 
 import java.util.Scanner;
 
+/**
+ * main 메서드
+ * param :
+ * return :
+ */
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = sc.nextInt();
-
+        int num1 = sc.nextInt(); // 첫 번쨰 숫자를 입력 받아 int타입 num1에 대입
 
         System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num2 = sc.nextInt();
+        int num2 = sc.nextInt(); // 두 번쨰 숫자를 입력 받아 int타입 num2에 대입
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0); // charAt(idx)는 문자열에서 매개변수로 받은 주소 값의 문자를 반환한다.
 
         System.out.println("num1 값 : "+num1);
         System.out.println("num2 값 : "+num2);
+        System.out.println("operator : "+operator);
     }
 }
