ee5b69c96e064294c4d25845ea1565e0f6efbf6c
seungsuuu <tmdtn013@naver.com>
2024-04-29 11:35:13 +0900

    level1 - 1 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,7 +1,21 @@
 package main.java.calculator;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int num1 = sc.nextInt();
+
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int num2 = sc.nextInt();
+
+        System.out.println("num1 값 : "+num1);
+        System.out.println("num2 값 : "+num2);
     }
 }
