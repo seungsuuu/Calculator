79a8bfde220ca1f91d69a02357027f86158c29fe
seungsuuu <tmdtn013@naver.com>
2024-04-29 19:15:27 +0900

    level2 - 2 App 클래스의 main 메서드에서 Calculator 클래스 생성하고 메서드를 사용하도록 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,8 +11,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator(); // Calculator 객체 생성
         boolean exitCheck = true; // 반복분 체크를 위한 boolean 변수 선언
-        ArrayList<Integer> resultList = new ArrayList<>(); // 길이가 고정되지 않은 동적 자료구조 ArrayList 선언 및 생성
 
         while (exitCheck) { // exitCheck를 체크해 while 반복문을 수행
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -27,36 +27,24 @@ public class App {
             System.out.println("입력 받은 연산 : " + num1 + operator + num2);
 
             int result = 0;
-            switch (operator) { // 입력받은 연산자(operator) 값에 따라 다르게 연산
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) { // 나눗셈은 0으로 나눌 수 없으므로 나누려는 num2 값에 0이면 프로그램 종료
-                        System.out.println("나눗셈의 경우 0으로 나눌 수 없습니다.");
-                        return;
-                    } else result = num1 / num2;
-                    break;
+            try { // calculate메서드 사용을 위한 throws 예외처리구문
+                result = cal.calculate(num1, num2,operator);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
             System.out.println("연산 결과 : " + result);
-            resultList.add(result); // 선언 생성된 ArrayList에 연산 결과 저장
+            cal.resultArr.add(result);
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removeSign = sc.next();
             if (removeSign.equals("remove")) { //입력된 문자열이 "remove"면 ArrayList의 첫 번째 요소 삭제
-                resultList.remove(0);
+                cal.resultArr.remove(0);
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquirySign = sc.next();
             if (inquirySign.equals("inquiry")) { //입력된 문자열이 "inquiry"면 ArrayList의 모든 요소 조회
-                for(int i: resultList) {
+                for(int i: cal.resultArr) {
                     System.out.println("저장된 연산결과 : " + i);
                 }
             }
