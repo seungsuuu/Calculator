f5f4e4ba6be378b7255f1f6b92b600336e39f353
seungsuuu <tmdtn013@naver.com>
2024-04-30 13:33:27 +0900

    level2 - 7 Calculator 클래스에 원의 넓비 구하는 연산 메서드 추가 및 알맞게 동작을 위한 App클래스 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,36 +14,60 @@ public class App {
         boolean exitCheck = true; // 반복분 체크를 위한 boolean 변수 선언
 
         while (exitCheck) { // exitCheck를 체크해 while 반복문을 수행
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt(); // 첫 번쨰 숫자를 입력 받아 int타입 num1에 대입
+            System.out.println("사칙연산 계산(operation 입력), 반지름 계산(circle 입력) 둘 중 하나를 입력해주세요.");
+            String calculatorType = sc.next();
+            System.out.println("입력한 타입 : "+calculatorType);
+            if (calculatorType.equals("operation")) { // 사칙연산 계산기
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt(); // 첫 번쨰 숫자를 입력 받아 int타입 num1에 대입
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt(); // 두 번쨰 숫자를 입력 받아 int타입 num2에 대입
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt(); // 두 번쨰 숫자를 입력 받아 int타입 num2에 대입
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0); // charAt(idx)는 문자열에서 매개변수로 받은 주소 값의 문자를 반환한다.
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0); // charAt(idx)는 문자열에서 매개변수로 받은 주소 값의 문자를 반환한다.
 
-            System.out.println("입력 받은 연산 : " + num1 + operator + num2);
+                System.out.println("입력 받은 연산 : " + num1 + operator + num2);
 
-            int result = 0;
-            try { // calculate메서드 사용을 위한 throws 예외처리구문
-                result = cal.calculate(num1, num2,operator);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            System.out.println("연산 결과 : " + result);
-            cal.addResult(result); // addResult 메서드 호출 접근해서 연산결과 추가
+                int resultInt = 0;
+                try { // calculate메서드 사용을 위한 throws 예외처리구문
+                    resultInt = cal.calculate(num1, num2, operator);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("연산 결과 : " + resultInt);
+                cal.addResult(resultInt); // addResult 메서드 호출 접근해서 연산결과 추가
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String removeSign = sc.next();
-            if (removeSign.equals("remove")) { //입력된 문자열이 "remove"면 ArrayList의 첫 번째 요소 삭제
-                cal.removeResult(); // removeResultArr 메서드로 호출
-            }
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String removeSign = sc.next();
+                if (removeSign.equals("remove")) { //입력된 문자열이 "remove"면 ArrayList의 첫 번째 요소 삭제
+                    cal.removeResult(); // removeResultArr 메서드로 호출
+                }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquirySign = sc.next();
-            if (inquirySign.equals("inquiry")) {//입력된 문자열이 "inquiry"면 inquiryResults 메서드 호출
-                cal.inquiryResults();
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquirySign = sc.next();
+                if (inquirySign.equals("inquiry")) {//입력된 문자열이 "inquiry"면 inquiryResults 메서드 호출
+                    cal.inquiryResults();
+                }
+            } else if (calculatorType.equals("circle")) { // 원의 넓비 계산기
+                System.out.print("원의 반지름을 입력하세요: ");
+                double radius = sc.nextDouble();
+                System.out.println("입력 받은 원의 반지름 : " + radius);
+
+                double resultDouble = cal.calculateCircleArea(radius);
+                System.out.println("원의 넓이 결과 : " + resultDouble);
+
+                cal.addResultDouble(resultDouble); // addResultDouble 메서드 호출 접근해서 연산결과 추가
+
+                System.out.println("저장된 원의 넓이 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquirySign = sc.next();
+                if (inquirySign.equals("inquiry")) {//입력된 문자열이 "inquiry"면 inquiryResultsDouble 메서드 호출
+                    cal.inquiryResultsDouble();
+                }
+
+            } else {
+                System.out.println("잘못된 입력입니다.");
+                continue;
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
